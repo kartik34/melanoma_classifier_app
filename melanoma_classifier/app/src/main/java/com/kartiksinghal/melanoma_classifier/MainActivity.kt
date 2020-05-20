666e5f9ce9a303113c0ac0373efd432df472727e
Kartik Singhal <kartiksinghal3.14@gmail.com>
2020-05-20 04:02:21 -0400

    replaced with kotlin boilerplate

---
+++ melanoma_classifier/app/src/main/java/com/kartiksinghal/melanoma_classifier/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.kartiksinghal.melanoma_classifier
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
